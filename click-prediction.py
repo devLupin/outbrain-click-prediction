7732ae302d7835c5768d1847cb584fe95609938b
Hyeontaek-Lim <luckyquit49@gmail.com>
2019-01-10 17:48:33 +0900

    V2

--- click-prediction.py
+++ click-prediction.py
@@ -19,3 +19,19 @@ print('# File sizes')
 for f in os.listdir('../input'):
     if 'zip' not in f:
         print(f.ljust(30) + str(round(os.path.getsize('../input/' + f) / 1000000, 2)) + 'MB')
+
+        
+df_train = pd.read_csv('../input/clicks_train.csv')
+df_test = pd.read_csv('../input/clicks_test.csv')
+
+sizes_train = df_train.groupby('display_id')['ad_id'].count().value_counts()
+sizes_test = df_test.groupby('display_id')['ad_id'].count().value_counts()
+sizes_train = sizes_train / np.sum(sizes_train)
+sizes_test = sizes_test / np.sum(sizes_test)
+
+plt.figure(figsize=(12,4))
+sns.barplot(sizes_train.index, sizes_train.values, alpha=0.8, color=p[0], label='train')
+sns.barplot(sizes_test.index, sizes_test.values, alpha=0.6, color=p[1], label='test')
+plt.legend()
+plt.xlabel('Number of Ads in display', fontsize=12)
+plt.ylabel('Proportion of set', fontsize=12)
